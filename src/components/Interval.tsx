ec20eceab7eebc094fdb61a35f9fa1822283f32e
David Andersson <lejonmanen@gmail.com>
2022-05-06 09:51:46 +0200

    En bug i intervallkomponenten

--- src/components/Interval.tsx
+++ src/components/Interval.tsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 
 const Interval = () => {
-  const [count, setCount] = useState<number>(1);
+	const [count, setCount] = useState<number>(1);
+	const [intervalId, setIntervalId] = useState<number>(0)
+
+	const stopFunction = () => {
+		console.log('Stoppar intervallet ', intervalId)
+		clearInterval(intervalId)
+	}
 
 	useEffect(() => {
-		let intervalId = setInterval(() => {
+		let id = setInterval(() => {
 			setCount(count => count + 1)
 		}, 500)
-		const stopFunction = () => clearInterval(intervalId);
+		setIntervalId(id)
+		console.log('Startar intervall... id', id)
+
+		// const stopFunction = () => {
+		// 	console.log('Stoppar intervallet...')
+		// 	clearInterval(intervalId)
+		// };
 		return stopFunction
 	}, [])
 
@@ -16,7 +28,7 @@ const Interval = () => {
     <div>
       <h3> Interval </h3>
       <p>
-        <button> Stop </button>
+        <button onClick={stopFunction}> Stop </button>
       </p>
       <p> {count} </p>
     </div>
